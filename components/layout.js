0ee2536a371ba4ff99463e3079079ec918eb126b
Faishal Irawan <muhammadfaishal193@gmail.com>
2021-02-23 18:29:36 +0700

    Add mobile menu

--- components/layout.js
+++ components/layout.js
@@ -1,36 +1,63 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Layout({ children }) {
+  const [nav, setNav] = useState(false)
+
   return (
-    <div className="text-white text-opacity-95 font-sans px-6 mx-auto my-0 sm:max-w-3xl 2xl:max-w-4xl">
+    <div className="text-white text-opacity-95 font-sans px-6 mx-auto my-0 relative sm:max-w-3xl 2xl:max-w-4xl">
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav className="py-8 flex justify-between">
-        <Link href="/">
-          <a>
-            <img 
-              src="/papurika black.svg"
-              className="h-14"
-              alt="Website Logo" />
-          </a>
-        </Link>
-        <button type="button" className="md:hidden">
-          <img src="/nav white.svg" className="w-10" alt="Hamburger Menu Button"/>
-        </button>
-        <ul className="hidden md:flex text-lg">
-          <li className="mr-14">
-            <Link href="/blog">
-              <a>Blog</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="">
-              <a>Works</a>
-            </Link>
-          </li>
-        </ul>
+      <nav className="relative">
+        <div className="py-8 flex justify-between">
+          <Link href="/">
+            <a>
+              <img 
+                src="/papurika black.svg"
+                className="h-14"
+                alt="Website Logo" />
+            </a>
+          </Link>                
+          {nav ? (
+            <button type="button" className="md:hidden" onClick={() => setNav(false)}>
+              <img src="/nav x.svg" className="w-8" alt="Hamburger Menu Button"/>
+            </button>
+          ) : (
+            <button type="button" className="md:hidden" onClick={() => setNav(true)}>
+              <img src="/nav white.svg" className="w-8" alt="Hamburger Menu Button"/>
+            </button>
+          )}        
+          <ul className="hidden md:flex text-lg">
+            <li className="mr-14">
+              <Link href="/blog">
+                <a>Blog</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="">
+                <a>Works</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        {nav &&      
+          <div>
+            <ul className="text-4xl flex flex-col items-center font-bold w-full absolute bg-dark-blue pb-10">
+              <li className="mb-8">
+                <Link href="/blog">
+                  <a>Blog</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="">
+                  <a>Works</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        }
       </nav>
       <main>
           { children }
