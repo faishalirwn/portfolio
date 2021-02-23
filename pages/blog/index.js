fc17defd46974dd2f87e9bc379a31228af2f6e7f
Faishal Irawan <muhammadfaishal193@gmail.com>
2021-02-23 19:23:35 +0700

    Improve blog list page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -7,23 +7,39 @@ import { getSortedPostsData } from '../../lib/posts'
 export default function Blog({ allPostsData }) {
   return (
     <Layout>
+        <Head>
+          <title>Blog</title>
+        </Head>
         <div className="md:px-20">
-            <h1 className="font-heading text-5xl text-white text-opacity-50 md:text-7xl">Blog</h1>
-            <div className="mt-10 flex flex-col md:flex-row md:flex-wrap md:justify-center">                
+            <h1 className="blog-entry-bg font-heading text-5xl text-white text-opacity-50 md:text-7xl">Blog</h1>
+            <div className="mt-10 flex flex-col md:flex-row md:flex-wrap md:justify-center">
                 {allPostsData.map(({ id, title, coverImage, date }) => (
                     <Link href={`/blog/${id}`}>
                         <a>
-                            <div className="blog-entry">
-                                <h3 className="blog-entry-title">{title}</h3>
-                                <div className="blog-entry-time">
-                                  <Date dateString={date} />
-                                </div>
-                            </div>
+                          <div className="blog-entry relative blog-entry-bg">
+                              <h3 className="blog-entry-title">{title}</h3>
+                              <div className="blog-entry-time">
+                                <Date dateString={date} />
+                              </div>
+                          </div>
+                          <style jsx>{`
+                          .blog-entry-bg:before {
+                            content: "";
+                            background-image: url('${coverImage}');
+                            background-size: cover;
+                            position: absolute;
+                            top: 0px;
+                            right: 0px;
+                            bottom: 0px;
+                            left: 0px;
+                            opacity: 0.2;
+                          }
+                        `}</style>
                         </a>
                     </Link>
                 ))}
             </div>
-        </div>
+        </div>        
     </Layout>
   )
 }
