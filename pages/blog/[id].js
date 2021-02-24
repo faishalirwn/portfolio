8c30d482e1f91be63bf54d11d2e9ab99eb541763
Faishal Irawan <muhammadfaishal193@gmail.com>
2021-02-24 20:31:55 +0700

    Change some style

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -12,7 +12,7 @@ export default function Post({ postData }) {
       </Head>
       <motion.article initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
         <h1 className="font-heading text-4xl mb-2 lg:text-5xl">{postData.title}</h1>
-        <div className="text-white text-opacity-75 mb-10">
+        <div className="text-white text-opacity-80 mb-10">
           <Date dateString={postData.date} />
         </div>
         <div className="mb-12 max-h-480px">
--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,11 @@ export default function Home() {
           <h1 className="font-heading text-5xl md:text-7xl md:mr-20">Faishal</h1>
           <h1 className="font-heading text-5xl ml-14 md:text-7xl md:ml-20">Irawan</h1>
         </div>
-        <p className="mb-10 text-2xl text-white text-opacity-100 md:text-3xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid</p>
+        <p className="mb-10 text-2xl text-white text-opacity-75 md:text-3xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim aliquid</p>
         <div className="text-2xl flex flex-wrap md:justify-center">
-          <a href="https://www.linkedin.com/in/faishalirwn/" className="mr-8 link-hover">LinkedIn</a>
-          <a href="https://github.com/faishalirwn" className="mr-8 link-hover">Github</a>
-          <a href="mailto:faishalirwn@pm.me" className="link-hover">Email</a>
+          <a href="https://www.linkedin.com/in/faishalirwn/" className="mr-8 social-link">LinkedIn</a>
+          <a href="https://github.com/faishalirwn" className="mr-8 social-link">Github</a>
+          <a href="mailto:faishalirwn@pm.me" className="social-link">Email</a>
         </div>
       </motion.div>
     </Layout>
