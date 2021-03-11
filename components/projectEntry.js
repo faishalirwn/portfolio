87d893363e8d32bf3ba0462293da073868b4798d
Faishal Irawan <muhammadfaishal193@gmail.com>
2021-03-11 15:10:11 +0700

    Change external icon from svg to img

--- components/projectEntry.js
+++ components/projectEntry.js
@@ -13,15 +13,13 @@ export default function ProjectEntry({ title, coverImage, demoUrl, desc }) {
               />
           </div>
           <div className="p-6">
+            <div className="flex">
               <h3 className="font-bold text-2xl">
               {title}
-              <svg className="ml-3 -mt-2 inline-block" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link">
-                  <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                  <polyline points="15 3 21 3 21 9"></polyline>
-                  <line x1="10" y1="14" x2="21" y2="3"></line>
-              </svg>
+              <img src="/icons/external-link.svg" className="ml-3 -mt-2 inline" alt="External Link Icon"/>
               </h3>
-              <p className="text-white text-opacity-50">{desc}</p>
+            </div>
+            <p className="text-white text-opacity-50">{desc}</p>
           </div>
         </div>
     </a>
